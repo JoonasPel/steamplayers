5c1923be5b7985190c1d0273d37579ae0eb94537
Joonas Pelttari <joonas.pelttari@gmail.com>
2025-01-11 21:30:50 +0200

    enable screenshots

--- tests/e2e/cypress.config.js
+++ tests/e2e/cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     supportFile: false,
-    screenshotOnRunFailure: false,
+    screenshotOnRunFailure: true,
     env: {
       baseUrl: "https://steamplayers.info",
     },
